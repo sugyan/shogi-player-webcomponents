6fd4bc766841eb75718ca0079fbc0ef6d23c14e6
sugyan <sugi1982@gmail.com>
2022-03-28 14:15:36 +0900

    Fix tests

--- src/test/shogi-player_test.ts
+++ src/test/shogi-player_test.ts
@@ -4,25 +4,19 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import {ShogiPlayer} from '../shogi-player.js';
+import { ShogiPlayer } from "../shogi-player.js";
 
-import {fixture, assert} from '@open-wc/testing';
-import {html} from 'lit/static-html.js';
+import { fixture, assert } from "@open-wc/testing";
+import { html } from "lit/static-html.js";
 
-suite('shogi-player', () => {
-  test('is defined', () => {
-    const el = document.createElement('shogi-player');
+suite("shogi-player", () => {
+  test("is defined", () => {
+    const el = document.createElement("shogi-player");
     assert.instanceOf(el, ShogiPlayer);
   });
 
-  test('renders with default values', async () => {
+  test("renders with default values", async () => {
     const el = await fixture(html`<shogi-player></shogi-player>`);
-    assert.shadowDom.equal(
-      el,
-      `
-      <h1>Hello, Shogi Player!</h1>
-      <slot></slot>
-    `
-    );
+    assert.dom.equal(el.shadowRoot?.children[0], `<div></div>`);
   });
 });
