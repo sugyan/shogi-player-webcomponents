517a87ed4fbf93546eef3228bdd94ed3c650fd23
sugyan <sugi1982@gmail.com>
2022-04-03 23:49:06 +0900

    Update sfen.ts

--- src/sfen.ts
+++ src/sfen.ts
@@ -33,28 +33,26 @@ const SFEN_PIECE_MAP: { [key: string]: Piece } = {
 
 export function parseSfen(sfen: string): [Board, Hand, Hand] {
   const board = Array.from(Array(9), () => Array(9).fill(null));
-  const hands = [
-    {
-      [PieceType.FU]: 0,
-      [PieceType.KY]: 0,
-      [PieceType.KE]: 0,
-      [PieceType.GI]: 0,
-      [PieceType.KI]: 0,
-      [PieceType.KA]: 0,
-      [PieceType.HI]: 0,
-      [PieceType.OU]: 0,
-    },
-    {
-      [PieceType.FU]: 0,
-      [PieceType.KY]: 0,
-      [PieceType.KE]: 0,
-      [PieceType.GI]: 0,
-      [PieceType.KI]: 0,
-      [PieceType.KA]: 0,
-      [PieceType.HI]: 0,
-      [PieceType.OU]: 0,
-    },
-  ];
+  const hand_black = {
+    [PieceType.FU]: 0,
+    [PieceType.KY]: 0,
+    [PieceType.KE]: 0,
+    [PieceType.GI]: 0,
+    [PieceType.KI]: 0,
+    [PieceType.KA]: 0,
+    [PieceType.HI]: 0,
+    [PieceType.OU]: 0,
+  };
+  const hand_white = {
+    [PieceType.FU]: 0,
+    [PieceType.KY]: 0,
+    [PieceType.KE]: 0,
+    [PieceType.GI]: 0,
+    [PieceType.KI]: 0,
+    [PieceType.KA]: 0,
+    [PieceType.HI]: 0,
+    [PieceType.OU]: 0,
+  };
   const parts = sfen.split(" ");
   const rows = parts[0].split("/", 9);
   if (rows.length !== 9) {
@@ -78,6 +76,35 @@ export function parseSfen(sfen: string): [Board, Hand, Hand] {
       throw new Error(`invalid sfen row: ${row}`);
     }
   });
-  // TODO: hands
-  return [board, hands[0], hands[1]];
+  // TODO: side to move
+  if (parts[2] !== "-") {
+    for (const m of parts[2].matchAll(/(\d*)([^\d])/g)) {
+      const num = Number(m[1] || "1");
+      const piece = SFEN_PIECE_MAP[m[2]];
+      if (!piece) {
+        throw new Error(`invalid sfen piece: ${m[2]}`);
+      }
+      const pt = piece.pieceType;
+      if (
+        pt !== PieceType.FU &&
+        pt !== PieceType.KY &&
+        pt !== PieceType.KE &&
+        pt !== PieceType.GI &&
+        pt !== PieceType.KI &&
+        pt !== PieceType.KA &&
+        pt !== PieceType.HI
+      ) {
+        throw new Error(`invalid hand piece: ${m[2]}`);
+      }
+      switch (piece.color) {
+        case Color.Black:
+          hand_black[pt] += num;
+          break;
+        case Color.White:
+          hand_white[pt] += num;
+          break;
+      }
+    }
+  }
+  return [board, hand_black, hand_white];
 }
