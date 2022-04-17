1ccadd8d4f40afe420da46b61baba7a15643d1bf
sugyan <sugi1982@gmail.com>
2022-04-17 10:51:32 +0900

    Update docs, package.json

--- docs-src/_includes/header.11ty.cjs
+++ docs-src/_includes/header.11ty.cjs
@@ -2,6 +2,6 @@ module.exports = function (data) {
   return `
 <header>
   <h1>&lt;shogi-player></h1>
-  <h2>A web component just for me.</h2>
+  <h2>A web component for shogi game.</h2>
 </header>`;
 };
